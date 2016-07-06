f46ed87f1ce8c01c5c9f7f7908d83b3621a3e730
Thomas Anderson Feitosa Monteiro <tafm@WINDOWS.CIN.UFPE.BR>
2016-07-06 16:08:26 -0300

    separando cliente do servidor

--- src/br/mafia/client/conexao/FalhaLoginException.java
+++
@@ -1,7 +0,0 @@
-package br.mafia.client.conexao;
-
-public class FalhaLoginException extends Exception {
-	public FalhaLoginException() {
-		super("Usuário ou senha incorretos");
-	}
-}
--- src/br/mafia/client/conexao/UsuarioJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package br.mafia.client.conexao;
-
-public class UsuarioJaCadastradoException extends Exception {
-	public UsuarioJaCadastradoException() {
-		super("Nome de usuário já cadastrado");
-	}
-}
